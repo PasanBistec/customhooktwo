cd6d0620bf91836f37a73fa3be3a36b365a7e49c
PasanBistec <pasan@bistecglobal.com>
2022-10-27 17:17:00 +0530

    using parameters with hooks

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,17 +1,17 @@
 import {useState} from 'react'
 
-function useCounter() {
-    const [count, setCount] = useState(0);
+function useCounter(initialvalue=0,value) {
+    const [count, setCount] = useState(initialvalue);
 
     const increment = () => {
-        setCount(prevCount => prevCount + 1)
+        setCount(prevCount => prevCount + value)
     }
     const decrement = () => {
-        setCount(prevCount => prevCount - 1)
+        setCount(prevCount => prevCount - value)
     }
 
     const reset = () => {
-        setCount(0)
+        setCount(initialvalue)
     }
 
     return [count,increment,decrement,reset]
